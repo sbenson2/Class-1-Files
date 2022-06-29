1dc1693be9b5c9d4b9dc341bae97f397009d2e32
Shawn W. Benson <46143954+sbenson2@users.noreply.github.com>
2022-06-28 19:59:06 -0700

    First Change

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
-alert();
+//this is how you create comments in javascript 
+
+//variables 
+
+Boolean = true; //this is true and false basically. Greate for controlling states of objects.
+
+count = 0; //integer....
+
+string = 'Hello world'; //this is a string. 
+
+
