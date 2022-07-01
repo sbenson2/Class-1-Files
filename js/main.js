11a8bf1539100bbb62f2a814aa00c70ae30349f3
Shawn W. Benson <46143954+sbenson2@users.noreply.github.com>
2022-06-30 17:15:10 -0700

    Finished.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,33 @@
 //this is how you create comments in javascript 
 
-//variables 
-
-Boolean = true; //this is true and false basically. Greate for controlling states of objects.
-
-count = 0; //integer....
-
-string = 'Hello world'; //this is a string. 
-
-
-PersonalName = "Hi, my name is Wesley Benson"; //my name
-PersnoalFood = "and my favorite food is Ice Cream!" //my favorite food
-
-console.log(PersonalName);
-console.log(PersnoalFood);
-
 /*
 
 block comment using these. 
 
 */
+
+//variables 
+
+var Boolean = true; //this is true and false basically. Greate for controlling states of objects.
+
+var count = 0; //integer....
+
+var string = 'Hello world'; //this is a string. 
+
+var PersonalName = "Hi, my name is Wesley Benson"; //my name
+var PersnoalFood = "and my favorite food is Ice Cream!"; //my favorite food
+
+//These are not used any more. 
+//console.log(PersonalName);
+//console.log(PersnoalFood);
+
+var BigString = "Supercalifragilisticexpialidocious";
+var AnyNumber = 1.34;
+var SomeBigInt = BigInt(10);
+
+console.log(BigString.length);
+console.log(typeof AnyNumber);
+console.log(typeof SomeBigInt);
+
+
+
